3aaf2412f880d1b1c3471e5e66c2f3569293248a
mangionegiulio <giuliofrancesco.mangione@gmail.com>
2026-05-29 16:41:00 +0200

    Completed the database.py file

--- marketplace_backend/app/core/database.py
+++ marketplace_backend/app/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import declerative_base
+from sqlalchemy import declarative_base
 from app.core.config import settings
 
 #Creo il motore di connessione al db
@@ -10,3 +10,11 @@ motore_database= create_engine(settings.DATABASE_URL)
 SessionLocal= sessionmaker(autocommit=False, autoflush=False, bind=motore_database)
 #Rimuovendo l'autocommit si ha il pieno controllo del salvataggio delle operazioni
 #Autoflush prepara le informazioni per l'autocommit, tenerlo attivo non ha senso
+Base= declarative_base()
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db  #indipendentemente dal fatto che la funzione sia finita o meno(crash), il db si chiude
+    finally:
+        db.close()
