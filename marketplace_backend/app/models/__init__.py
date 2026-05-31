12695c86dcf8efba1e475dc0c9f322ad73438d94
Marco Mazzone <marco.mazzone06@gmail.com>
2026-05-31 14:09:08 +0200

    implemented init file into model directory

---
+++ marketplace_backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.base import Base
+from app.models.admin import Admin
+from app.models.categories import Categories
+from app.models.products import Product
+from app.models.product_variants import ProductVariants
