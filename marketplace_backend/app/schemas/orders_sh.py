c5b891a49f18d10b3aa9c5646db56e75a33e031e
mangionegiulio <giuliofrancesco.mangione@gmail.com>
2026-06-03 19:33:50 +0200

    Implemented orders schema

---
+++ marketplace_backend/app/schemas/orders_sh.py
@@ -0,0 +1,39 @@
+from pydantic.main import BaseModel
+from pydantic.config import ConfigDict
+from pydantic.fields import Field
+from app.models.orders import  FulfillmentStatusEnum, PaymentStatusEnum
+from uuid import UUID
+from datetime import datetime
+
+
+class Address:
+    pass  #Da implementare con i campi necessari (street, city, state, zip_code, country, azienda, partita_iva, etc.)
+
+
+class OrderCreate(BaseModel):
+    shipping_address : Address
+    billing_address : Address
+    notes: str | None = Field(None, max_length=1000, description="Optional notes for the order, max length 1000 characters")
+
+
+class OrderUpdate(BaseModel):
+    payment_status: PaymentStatusEnum | None = Field(None, description="Updated payment status for the order")
+    fulfillment_status: FulfillmentStatusEnum | None = Field(None, description="Updated fulfillment status for the order")
+    notes: str | None = Field(None, max_length=1000, description="Optional notes for the order, max length 1000 characters")
+
+
+class OrderRead(BaseModel):
+    id: UUID
+    user_id: UUID | None
+    customer_email: str = Field(..., max_length=255)
+    shipping_address: Address
+    billing_address: Address
+    total_amount_cents: int
+    stripe_intent_id: str | None = Field(None, max_length=255)
+    payment_status: PaymentStatusEnum
+    fulfillment_status: FulfillmentStatusEnum
+    notes: str | None = Field(None, max_length=1000)
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
